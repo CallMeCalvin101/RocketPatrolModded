91053b3fa12efeb18a349cd397f4d06a226d1da9
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2022-04-17 19:39:39 -0700

    Added old rocket patrol

---
+++ src/main.js
@@ -0,0 +1,17 @@
+console.log("Hello from main.js");
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
+
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+//set UI sizes
+let borderUISize = config.height / 15;
+let borderPadding = borderUISize / 3;
+
+let game = new Phaser.Game(config);
