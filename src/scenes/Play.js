5a61ad2f1ffdfb5bf3fb668d0fe6621e0b2831c6
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2022-04-17 19:52:06 -0700

    test

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,38 +11,40 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-        // define keys
+        // Define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
+        // Background
         this.bg = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
 
+        // Players
         this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket');
 
-        // add spaceships (x3)
+        // Add spaceships (x3)
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
 
-        //Green Rectangle
+        // Green Rectangle
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
-        //White Border
+        // White Border
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 
-        //explosion
+        // Explosion
         this.anims.create({
             key: 'explode',
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
             frameRate: 30
         })
 
-        //initialize score
+        // Initialize score
         this.p1Score = 0;
 
         //displays score
@@ -64,7 +66,7 @@ class Play extends Phaser.Scene {
         //Game Over flag
         this.gameOver = false;
 
-        //play timer
+        //Play timer
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
